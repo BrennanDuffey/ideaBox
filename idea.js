9a1d566825392b23f1506c9c1bbb8499eafa372f
Kelly Zick <kllyzck@gmail.com>
2019-02-12 14:38:51 -0700

    Create Idea class

---
+++ idea.js
@@ -0,0 +1,8 @@
+class Idea {
+    constructor(id, title, body, quality) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.quality = quality || 'Swill';
+    }
+}
