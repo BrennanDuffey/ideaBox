c3547832fa3ab4d5b37e15da3c00724722b2bcbd
Menashe <moses2787@gmail.com>
2019-02-13 15:27:36 -0700

    Alphabetize CSS properties and JS variable names

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var searchInput = document.querySelector('.search-input');
-var newIdeaForm = document.querySelector('.new-idea-form');
-var titleInput = document.querySelector('#title-input');
 var bodyInput = document.querySelector('#body-input');
-var saveBtn = document.querySelector('.save-btn');
 var cardSection = document.querySelector('.card-section');
-var ideaCard = document.querySelector('.idea-card');
 var ideaArray = [];
+var ideaCard = document.querySelector('.idea-card');
+var newIdeaForm = document.querySelector('.new-idea-form');
+var saveBtn = document.querySelector('.save-btn');
+var searchInput = document.querySelector('.search-input');
+var titleInput = document.querySelector('#title-input');
 
 window.addEventListener('load', onPageLoad)
 saveBtn.addEventListener('click', saveIdea)
