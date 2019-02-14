36cb09aca4dbecb45afb33c70e2ab6c8d7fbd98e
Menashe <moses2787@gmail.com>
2019-02-14 11:20:50 -0700

    Refactor space on main.js, idea.js

--- idea.js
+++ idea.js
@@ -1,23 +1,23 @@
 class Idea {
-    constructor(id, title, body, quality) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-        this.quality = quality || 'GREAT!!';
-    }
-    saveToStorage(ideaArray) {
-        localStorage.setItem("storedIdeas", JSON.stringify(ideaArray));
-    }
+  constructor(id, title, body, quality) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+    this.quality = quality || 'Genius';
+  }
+  saveToStorage(ideaArray) {
+    localStorage.setItem("storedIdeas", JSON.stringify(ideaArray));
+  }
 
-    deleteFromStorage() {
+  deleteFromStorage() {
 
-    }
+  }
 
-    updateContent() {
+  updateContent() {
 
-    }
+  }
 
-    updateQuality() {
-      
-    }
+  updateQuality() {
+    
+  }
 }
--- main.js
+++ main.js
@@ -1,59 +1,58 @@
 var bodyInput = document.querySelector('#body-input');
 var cardSection = document.querySelector('.card-section');
-var ideaArray = [];
 var ideaCard = document.querySelector('.idea-card');
 var saveBtn = document.querySelector('.save-btn');
 var searchInput = document.querySelector('.search-input');
 var titleInput = document.querySelector('#title-input');
+var ideaArray = [];
 
 window.addEventListener('load', onPageLoad);
 saveBtn.addEventListener('click', saveIdea);
 
 function onPageLoad() {
-    var parsedArray = JSON.parse(localStorage.getItem("storedIdeas"));
-    parsedArray.forEach(function(idea) {
-        var newIdea = new Idea(idea.id, idea.title, idea.body, idea.quality);
-        appendCard(newIdea);
-        ideaArray.push(newIdea);
-        newIdea.saveToStorage(ideaArray);
-    });
+  var parsedArray = JSON.parse(localStorage.getItem("storedIdeas"));
+  parsedArray.forEach(function(idea) {
+    var newIdea = new Idea(idea.id, idea.title, idea.body, idea.quality);
+    appendCard(newIdea);
+    ideaArray.push(newIdea);
+  });
 }
 
 function saveIdea(e) {
-    e.preventDefault();
-    var newIdea = new Idea(Date.now(), titleInput.value, bodyInput.value);
-    appendCard(newIdea);
-    ideaArray.push(newIdea);
-    newIdea.saveToStorage(ideaArray);
-    clearInputs();
+  e.preventDefault();
+  var newIdea = new Idea(Date.now(), titleInput.value, bodyInput.value);
+  appendCard(newIdea);
+  ideaArray.push(newIdea);
+  newIdea.saveToStorage(ideaArray);
+  clearInputs();
 }
 
 function appendCard(idea) {
-    cardSection.innerHTML += 
-    `<article data-id=${idea.id} class="idea-card">
-        <div class="card-main">
-          <h2 class="card-title" contenteditable="true">
-            ${idea.title}
-          </h2>
-          <p class="card-body" contenteditable="true">
-            ${idea.body}
-          </p>
-        </div>
-        <div class="card-footer">
-          <img alt="upvote btn" src="images/upvote.svg" class="card-btn" id="upvote-btn">
-          <img alt="downvote btn" src="images/downvote.svg" class="card-btn" id="downvote-btn">
-          <p class="quality-label">
-            Quality: 
-            <span class="card-quality">${idea.quality}</span>
-          </p>
-          <img alt="delete btn" src="images/delete.svg" class="card-btn" id="delete-btn">
-        </div>
-      </article>`;
+  cardSection.innerHTML += 
+  `<article data-id=${idea.id} class="idea-card">
+    <div class="card-main">
+      <h2 class="card-title" contenteditable="true">
+        ${idea.title}
+      </h2>
+      <p class="card-body" contenteditable="true">
+        ${idea.body}
+      </p>
+    </div>
+    <div class="card-footer">
+      <img alt="upvote btn" src="images/upvote.svg" class="card-btn" id="upvote-btn">
+      <img alt="downvote btn" src="images/downvote.svg" class="card-btn" id="downvote-btn">
+      <p class="quality-label">
+        Quality: 
+        <span class="card-quality">${idea.quality}</span>
+      </p>
+      <img alt="delete btn" src="images/delete.svg" class="card-btn" id="delete-btn">
+    </div>
+  </article>`;
 }
 
 function clearInputs() {
-    titleInput.value = '';
-    bodyInput.value = '';
+  titleInput.value = '';
+  bodyInput.value = '';
 }
 
 // function newIdeaObject() {
